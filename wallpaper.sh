ca24357f7c63f25bae8eb36799f9be3b8a1655e4
Luis Alexandre Deschamps Brandão <luis.brandao@rentcars.com>
2017-12-20 19:19:42 -0200

    All images should be in jpg

--- wallpaper.sh
+++ wallpaper.sh
@@ -26,23 +26,23 @@ fi
 
 roleta=$(( ( RANDOM % 3 )  + 1 ))
 case "${roleta}" in
-  1)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry01.png'
+  1)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry01.jpg'
       ;;
-  2)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry02.png'
+  2)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry02.jpg'
       ;;
-  3)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry03.png'
+  3)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/terry03.jpg'
       ;;
-  4)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage01.png'
+  4)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage01.jpg'
       ;;
-  5)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage02.png'
+  5)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage02.jpg'
       ;;
-  6)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage03.png'
+  6)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/cage03.jpg'
       ;;
-  7)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber01.png'
+  7)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber01.jpg'
       ;;
-  8)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber02.png'
+  8)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber02.jpg'
       ;;
-  9)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber03.png'
+  9)  imagem='https://raw.githubusercontent.com/luisbrandao/funhouse/master/Images/bieber03.jpg'
       ;;
 esac
 
